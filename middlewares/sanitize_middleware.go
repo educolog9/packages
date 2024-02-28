fd5bf9c1e75abaf7175e9725d40df398ff9c44d7
bujosa <davidbujosa@gmail.com>
2024-02-28 00:12:54 -0400

    feat: implement sanitize middleware

---
+++ middlewares/sanitize_middleware.go
@@ -0,0 +1,47 @@
+package middlewares
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/educolog7/packages/errors/messages"
+	"github.com/educolog7/packages/sanitize"
+	"github.com/educolog7/packages/types"
+	"github.com/gin-gonic/gin"
+)
+
+// SanitizeMiddleware is a middleware that sanitizes input to prevent XSS attacks
+func SanitizeMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Method != http.MethodGet {
+			body, err := io.ReadAll(c.Request.Body)
+			if err != nil {
+				response := types.ErrorResponse{
+					Status:  http.StatusBadRequest,
+					Message: messages.BadRequest,
+					Errors:  []string{err.Error()},
+				}
+				c.AbortWithStatusJSON(http.StatusBadRequest, response)
+				return
+			}
+
+			// Sanitize the body
+			sanitizedBody, err := sanitize.SanitizeJSON(body)
+			if err != nil {
+				response := types.ErrorResponse{
+					Status:  http.StatusBadRequest,
+					Message: messages.BadRequest,
+					Errors:  []string{err.Error()},
+				}
+				c.AbortWithStatusJSON(http.StatusBadRequest, response)
+				return
+			}
+
+			// Replace the request body with the sanitized body
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(sanitizedBody))
+		}
+
+		c.Next()
+	}
+}
