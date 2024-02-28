ebee6c916c98244d73a1e97491d85c66c1a63b2a
bujosa <davidbujosa@gmail.com>
2024-02-28 00:13:26 -0400

    feat: implement parse pagination params

---
+++ middlewares/parse_pagination_params.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/educolog7/packages/functions"
+	"github.com/gin-gonic/gin"
+)
+
+// ParsePaginationParams is a middleware function that parses pagination parameters from the request.
+// It extracts the pagination parameters from the request and sets them in the context for further processing.
+// If there is an error while parsing the parameters, it returns a bad request error.
+func ParsePaginationParams() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		pagination, err := functions.ParsePaginationParams(c)
+		if err != nil {
+			functions.HandleErrorWithHttpStatus(c, http.StatusBadRequest, err)
+			c.Abort()
+			return
+		}
+
+		c.Set("pagination", pagination)
+		c.Next()
+	}
+}
