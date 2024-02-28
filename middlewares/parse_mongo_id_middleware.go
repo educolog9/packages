b1d0dfec6e7c61db53401d1ae3c04e40e94a855c
bujosa <davidbujosa@gmail.com>
2024-02-28 00:13:36 -0400

    feat: implement parse mongo id middleware

---
+++ middlewares/parse_mongo_id_middleware.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/educolog7/packages/types"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ParseMongoIDMiddleware is a middleware function that parses a MongoDB ObjectID from the request parameter and sets it in the context.
+// If the ID is invalid, it returns a JSON response with a status code of 400 Bad Request and an error message.
+// The middleware function is designed to be used with the Gin framework.
+func ParseMongoIDMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		idStr := c.Param("id")
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			response := types.ErrorResponse{
+				Status:  http.StatusBadRequest,
+				Message: "Invalid ID",
+				Errors:  []string{err.Error()},
+			}
+			c.JSON(http.StatusBadRequest, response)
+			c.Abort()
+			return
+		}
+
+		c.Set("mongoID", id)
+		c.Next()
+	}
+}
