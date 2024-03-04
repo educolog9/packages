da2835933c90b72308416111104c32af7c08c323
bujosa <davidbujosa@gmail.com>
2024-03-04 02:12:41 -0400

    feat(functions): add default pagination param

--- functions/parse_pagination_param.go
+++ functions/parse_pagination_param.go
@@ -13,22 +13,28 @@ import (
 func ParsePaginationParams(c *gin.Context) (*types.Pagination, error) {
 	paginationEncode := c.Query("p")
 
-	decodedData, err := base64.URLEncoding.DecodeString(paginationEncode)
-	if err != nil {
-		return nil, fmt.Errorf("invalid pagination format: %w", err)
-	}
-
 	var p types.Pagination
-	err = json.Unmarshal(decodedData, &p)
-	if err != nil {
-		return nil, fmt.Errorf("invalid pagination data: %w", err)
-	}
 
-	if p.Order == "" {
+	if paginationEncode == "" {
+		p.Limit = 10
 		p.Order = enums.Asc
 	} else {
-		if p.Order != enums.Asc && p.Order != enums.Desc {
-			return nil, fmt.Errorf("invalid order format")
+		decodedData, err := base64.URLEncoding.DecodeString(paginationEncode)
+		if err != nil {
+			return nil, fmt.Errorf("invalid pagination format: %w", err)
+		}
+
+		err = json.Unmarshal(decodedData, &p)
+		if err != nil {
+			return nil, fmt.Errorf("invalid pagination data: %w", err)
+		}
+
+		if p.Order == "" {
+			p.Order = enums.Asc
+		} else {
+			if p.Order != enums.Asc && p.Order != enums.Desc {
+				return nil, fmt.Errorf("invalid order format")
+			}
 		}
 	}
 
