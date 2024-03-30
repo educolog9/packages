290f6904a38442d81d207d87cea1a72e3e56a944
bujosa <davidbujosa@gmail.com>
2024-03-30 16:35:03 -0400

    feat: allow paramName in parse mongo id middleware

--- middlewares/parse_mongo_id_middleware.go
+++ middlewares/parse_mongo_id_middleware.go
@@ -11,9 +11,14 @@ import (
 // ParseMongoIDMiddleware is a middleware function that parses a MongoDB ObjectID from the request parameter and sets it in the context.
 // If the ID is invalid, it returns a JSON response with a status code of 400 Bad Request and an error message.
 // The middleware function is designed to be used with the Gin framework.
-func ParseMongoIDMiddleware() gin.HandlerFunc {
+func ParseMongoIDMiddleware(paramName ...string) gin.HandlerFunc {
+	param := "id"
+	if len(paramName) > 0 {
+		param = paramName[0]
+	}
+
 	return func(c *gin.Context) {
-		idStr := c.Param("id")
+		idStr := c.Param(param)
 		_, err := primitive.ObjectIDFromHex(idStr)
 		if err != nil {
 			response := types.ErrorResponse{
@@ -26,7 +31,7 @@ func ParseMongoIDMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		c.Set("id", idStr)
+		c.Set(param, idStr)
 		c.Next()
 	}
 }
