506df32a1515af47b50a84789c4c73ff00422234
bujosa <davidbujosa@gmail.com>
2024-03-30 02:28:34 -0400

    feat: implement translations for the validations

--- validations/validate_json.go
+++ validations/validate_json.go
@@ -6,25 +6,31 @@ import (
 	"github.com/educolog7/packages/errors/messages"
 	"github.com/educolog7/packages/types"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 )
 
-func ValidateJSON(obj interface{}) gin.HandlerFunc {
+func ValidateJSON(obj interface{}, trans ut.Translator) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var errors []string
 
+		if trans == nil {
+			en := en.New()
+			uni := ut.New(en, nil)
+			trans, _ = uni.GetTranslator("en")
+		}
+
 		if err := c.ShouldBind(obj); err != nil {
-
 			for _, err := range err.(validator.ValidationErrors) {
-				errors = append(errors, err.Error())
+				errors = append(errors, err.Translate(trans))
 			}
-
 		}
 
 		if err := Validate.Struct(obj); err != nil {
 			if errs, ok := err.(validator.ValidationErrors); ok {
 				for _, e := range errs {
-					errors = append(errors, e.Error())
+					errors = append(errors, e.Translate(trans))
 				}
 			} else {
 				errors = append(errors, err.Error())
--- validations/validation.go
+++ validations/validation.go
@@ -1,6 +1,11 @@
 package validations
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/es"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+)
 
 var Validate *validator.Validate
 
@@ -8,4 +13,125 @@ func init() {
 	// Initialize the validator and register the custom URL validation function
 	Validate = validator.New()
 	_ = Validate.RegisterValidation("customurl", validateURL)
+	_ = Validate.RegisterValidation("mongoID", validateMongoID)
+
+	// Initialize the translators
+	en := en.New()
+	es := es.New()
+
+	uni := ut.New(en, es)
+
+	// Set the translator for English
+	transEn, _ := uni.GetTranslator("en")
+	registerENTranslations(transEn)
+
+	// Set the translator for Spanish
+	transEs, _ := uni.GetTranslator("es")
+	registerESTranslations(transEs)
+}
+
+// registerENTranslations registers custom translations for validation tags in English.
+// It takes a `trans` ut.Translator as a parameter and adds translations for the "customurl" and "mongoID" validation tags.
+// The translations are added using the `Validate.RegisterTranslation` function, which takes a tag name, translator, and translation functions.
+// The translation functions are responsible for adding the translated error message for each validation tag.
+// The "customurl" translation function adds the error message "{0} is not a valid URL" for the "customurl" tag.
+// The "mongoID" translation function adds the error message "{0} is not a valid Mongo ID" for the "mongoID" tag.
+// This function does not return any value.
+func registerENTranslations(trans ut.Translator) {
+	_ = Validate.RegisterTranslation("customurl", trans, func(ut ut.Translator) error {
+		return ut.Add("customurl", "{0} is not a valid URL", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("customurl", fe.Field())
+		return t
+	})
+
+	// Add translations for other validation tags here
+	_ = Validate.RegisterTranslation("mongoID", trans, func(ut ut.Translator) error {
+		return ut.Add("mongoID", "{0} is not a valid Mongo ID", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("mongoID", fe.Field())
+		return t
+	})
+
+	// Add translations for the "required" tag
+	_ = Validate.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} is required", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("required", fe.Field())
+		return t
+	})
+
+	// Add translations for the "min" and "max" tags
+	_ = Validate.RegisterTranslation("min", trans, func(ut ut.Translator) error {
+		return ut.Add("min", "{0} must be at least {1}", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("min", fe.Field(), fe.Param())
+		return t
+	})
+
+	_ = Validate.RegisterTranslation("max", trans, func(ut ut.Translator) error {
+		return ut.Add("max", "{0} must be no more than {1}", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("max", fe.Field(), fe.Param())
+		return t
+	})
+
+	_ = Validate.RegisterTranslation("email", trans, func(ut ut.Translator) error {
+		return ut.Add("email", "{0} must be a valid email", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("email", fe.Field())
+		return t
+	})
+}
+
+// registerESTranslations registers custom translations for validation tags in the provided translator.
+// It adds translations for the "customurl" and "mongoID" validation tags.
+// The translations are used to provide localized error messages for validation failures.
+// The translator parameter should implement the ut.Translator interface.
+// The function returns an error if there was a problem registering the translations.
+func registerESTranslations(trans ut.Translator) {
+	_ = Validate.RegisterTranslation("customurl", trans, func(ut ut.Translator) error {
+		return ut.Add("customurl", "{0} no es una URL válida", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("customurl", fe.Field())
+		return t
+	})
+
+	// Add translations for other validation tags here
+	_ = Validate.RegisterTranslation("mongoID", trans, func(ut ut.Translator) error {
+		return ut.Add("mongoID", "{0} no es un ID de Mongo válido", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("mongoID", fe.Field())
+		return t
+	})
+
+	// Add translations for the "required" tag
+	_ = Validate.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} es requerido", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("required", fe.Field())
+		return t
+	})
+
+	// Add translations for the "min" and "max" tags
+	_ = Validate.RegisterTranslation("min", trans, func(ut ut.Translator) error {
+		return ut.Add("min", "{0} debe ser al menos {1}", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("min", fe.Field(), fe.Param())
+		return t
+	})
+
+	_ = Validate.RegisterTranslation("max", trans, func(ut ut.Translator) error {
+		return ut.Add("max", "{0} no debe ser más de {1}", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("max", fe.Field(), fe.Param())
+		return t
+	})
+
+	_ = Validate.RegisterTranslation("email", trans, func(ut ut.Translator) error {
+		return ut.Add("email", "{0} debe ser un correo electrónico válido", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("email", fe.Field())
+		return t
+	})
 }
