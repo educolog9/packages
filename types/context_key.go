a9bbe1259232c6b79a0cf4db4f6d156c6cd1c21a
bujosa <davidbujosa@gmail.com>
2024-04-09 01:11:17 -0400

    feat: implement content language key

---
+++ types/context_key.go
@@ -0,0 +1,7 @@
+package types
+
+type contextKey string
+
+// ContentLanguageKey is a context key used to store the content language.
+// It is used to retrieve the content language from the context.
+const ContentLanguageKey contextKey = "contentLanguage"
