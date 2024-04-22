8d068fb5e0cc5efdc5a051f168d4dddc53329a6a
bujosa <davidbujosa@gmail.com>
2024-04-22 01:12:58 -0400

    feat: add country code key

--- types/context_key.go
+++ types/context_key.go
@@ -5,3 +5,7 @@ type contextKey string
 // ContentLanguageKey is a context key used to store the content language.
 // It is used to retrieve the content language from the context.
 const ContentLanguageKey contextKey = "contentLanguage"
+
+// CountryCodeKey is a context key used to store the country code.
+// It is used to retrieve the country code from the context.
+const CountryCodeKey contextKey = "countryCode"
