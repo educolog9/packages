f474a99b447866faf1b2ce2f03a3a706c2fdc874
bujosa <davidbujosa@gmail.com>
2024-05-13 01:59:09 -0400

    feat: add convert pagination to mongo filter pipeline

--- databases/convert_pagination_to_mongo_filter.go
+++ databases/convert_pagination_to_mongo_filter.go
@@ -97,3 +97,85 @@ func ConvertPaginationToMongoFilter(config *types.PaginationConfig) (bson.M, *op
 
 	return filter, findOptions, nil
 }
+
+// ConvertPaginationToMongoPipeline converts a PaginationConfig into a MongoDB filter and pipeline.
+// It takes a PaginationConfig as input and returns a bson.M filter, []bson.M pipeline, and an error.
+func ConvertPaginationToMongoPipeline(config *types.PaginationConfig) (bson.M, []bson.M, error) {
+	pipeline := []bson.M{}
+
+	withLimit := config.WithLimit
+	pagination := config.Pagination
+
+	if withLimit {
+		pipeline = append(pipeline, bson.M{"$skip": pagination.GetOffset()})
+		pipeline = append(pipeline, bson.M{"$limit": pagination.GetLimit()})
+	}
+
+	if pagination.GetSort() != "" {
+		sortOrder := 1
+		if pagination.GetOrder() == "desc" {
+			sortOrder = -1
+		}
+		pipeline = append(pipeline, bson.M{"$sort": bson.M{pagination.GetSort(): sortOrder}})
+	}
+
+	filter := bson.M{}
+	if pagination.GetSearch() != "" {
+		filter = bson.M{"$text": bson.M{"$search": pagination.GetSearch()}}
+	}
+
+	for _, f := range pagination.GetFilters() {
+		value := f.Value
+		if reflect.TypeOf(f.Value).Kind() == reflect.String {
+			id, err := primitive.ObjectIDFromHex(f.Value.(string))
+			if err == nil {
+				value = id
+			}
+		}
+
+		switch f.Operator {
+		case enums.Equal:
+			filter[f.Field] = bson.M{"$eq": value}
+		case enums.NotEqual:
+			filter[f.Field] = bson.M{"$ne": value}
+		case enums.GreaterThan:
+			filter[f.Field] = bson.M{"$gt": value}
+		case enums.GreaterThanOrEqual:
+			filter[f.Field] = bson.M{"$gte": value}
+		case enums.LessThan:
+			filter[f.Field] = bson.M{"$lt": value}
+		case enums.LessThanOrEqual:
+			filter[f.Field] = bson.M{"$lte": value}
+		case enums.In:
+			switch v := value.(type) {
+			case []string:
+				filter[f.Field] = bson.M{"$in": v}
+			case []interface{}:
+				filter[f.Field] = bson.M{"$in": v}
+			case string:
+				filter[f.Field] = bson.M{"$in": []string{v}}
+			default:
+				return nil, nil, fmt.Errorf("invalid format for 'in' operator")
+			}
+		case enums.NotIn:
+			switch v := value.(type) {
+			case []string:
+				filter[f.Field] = bson.M{"$nin": v}
+			case []interface{}:
+				filter[f.Field] = bson.M{"$nin": v}
+			case string:
+				filter[f.Field] = bson.M{"$nin": []string{v}}
+			default:
+				return nil, nil, fmt.Errorf("invalid format for 'not in' operator")
+			}
+		case enums.Like:
+			filter[f.Field] = bson.M{"$regex": value}
+		case enums.NotLike:
+			filter[f.Field] = bson.M{"$not": bson.M{"$regex": value}}
+		default:
+			return nil, nil, fmt.Errorf("unsupported operator %s", f.Operator)
+		}
+	}
+
+	return filter, pipeline, nil
+}
