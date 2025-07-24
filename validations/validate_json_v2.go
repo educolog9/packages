766e18732237b15101bb90eca02c1805176d6f21
Jose Diaz <hello@josebdiaz.com>
2025-07-24 17:36:16 -0400

    feat: validation JSON v2

---
+++ validations/validate_json_v2.go
@@ -0,0 +1,57 @@
+package validations
+
+import (
+	"net/http"
+
+	"github.com/educolog9/packages/errors/messages"
+	languagues "github.com/educolog9/packages/languages"
+	"github.com/educolog9/packages/types"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func ValidateJSONV2(objFactory func() interface{}) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var errors []string
+
+		obj := objFactory() // Create a new instance per request
+
+		language, ok := c.Get("language")
+		if !ok {
+			language = languagues.Spanish.String()
+		}
+
+		trans, _ := Uni.GetTranslator(language.(string))
+
+		if err := c.ShouldBind(obj); err != nil {
+			for _, err := range err.(validator.ValidationErrors) {
+				errors = append(errors, err.Translate(trans))
+			}
+		}
+
+		if err := Validate.Struct(obj); err != nil {
+			if errs, ok := err.(validator.ValidationErrors); ok {
+				for _, e := range errs {
+					errors = append(errors, e.Translate(trans))
+				}
+			} else {
+				errors = append(errors, err.Error())
+			}
+		}
+
+		if len(errors) > 0 {
+			response := types.ErrorResponse{
+				Status:  http.StatusBadRequest,
+				Message: messages.ValidationFailed,
+				Errors:  errors,
+			}
+
+			c.JSON(http.StatusBadRequest, response)
+			c.Abort()
+			return
+		}
+
+		c.Set("validatedJSON", obj)
+		c.Next()
+	}
+}
