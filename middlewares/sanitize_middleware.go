deb318ca955c80edcc2c9fa51a45e79a2d7f59b9
jemsEducology <jems@educology.com>
2026-04-21 17:34:33 -0400

    Update sanitize_middleware.go

--- middlewares/sanitize_middleware.go
+++ middlewares/sanitize_middleware.go
@@ -15,7 +15,7 @@ import (
 // SanitizeMiddleware is a middleware that sanitizes input to prevent XSS attacks
 func SanitizeMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.Request.Method != http.MethodGet {
+		if c.Request.Method == http.MethodPost ||  c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPatch {
 			if !strings.HasPrefix(c.GetHeader("Content-Type"), "multipart/form-data") {
 				body, err := io.ReadAll(c.Request.Body)
 				if err != nil {
